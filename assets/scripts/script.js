9283cf27eb4a92cb41ea7e154aa668c0d994a893
Muhammad Idrees <idreeeeees1999@gmail.com>
2022-05-16 12:48:31 +0500

    v1

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+// menu btn
+function menufunction() {
+	var menu = document.getElementById("menu2");
+	if (menu.style.display === "none") {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+}
+
+// $(".owl-carousel").owlCarousel({
+// 	loop: true,
+// 	margin: 10,
+// 	nav: true,
+// 	responsive: {
+// 		0: {
+// 			items: 1,
+// 		},
+// 		600: {
+// 			items: 3,
+// 		},
+// 		1000: {
+// 			items: 5,
+// 		},
+// 	},
+// });
